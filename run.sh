9f9055e0557f8c68e96fbebaf682c1fa9ccc86a8
roshan pious <80870643+tanaka3146@users.noreply.github.com>
2023-04-08 16:38:06 +0530

    task

--- run.sh
+++ run.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-
 set -e
-
 export FLASK_APP=core/server.py
-
 rm -rf core/store.sqlite3
-
 flask db upgrade -d core/migrations/
-
 gunicorn -c gunicorn_config.py core.server:app
--- test.sh
+++ test.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-
-
 set -e
-
-
 export FLASK_APP=core/server.py
-
 rm -rf core/store.sqlite3
-
 flask db upgrade -d core/migrations/
-
 pytest -vvv -s tests
